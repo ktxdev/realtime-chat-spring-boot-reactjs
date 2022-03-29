c5cbd43c76f794f941b4b02ca3a4d56c36670bfc
ktxdev <sean.ktxdev@gmail.com>
2022-03-29 18:00:31 +0200

    Project Setup

---
+++ src/main/java/com/ktxdev/RealtimeChatSpringBootReactJSApplication.java
@@ -0,0 +1,13 @@
+package com.ktxdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatSpringBootReactJSApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(RealtimeChatSpringBootReactJSApplication.class, args);
+    }
+}
